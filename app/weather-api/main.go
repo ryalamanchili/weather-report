b9f06f87e50fd58c849161c9fe9754b62630b883
Derek <derekkenney@gmail.com>
2020-06-28 16:55:32 -0400

    Added DB configurations to the main function of app

--- app/weather-api/main.go
+++ app/weather-api/main.go
@@ -7,53 +7,47 @@ import (
 
 	"github.com/ardanlabs/service/foundation/database"
 	"github.com/gorilla/mux"
-	"github.com/pkg/errors"
 )
+
+type Config struct {
+	APIHost      string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	Handler      *mux.Router
+	DBUser       string
+	DBPassword   string
+	DBHost       string
+	DBName       string
+	DisableTLS   bool
+	ReporterURI  string
+	ServiceName  string
+	Probability  float64
+}
 
 // main is the entry point for the weather-report application
 func main() {
+
+	// Initialize the config.DB object
+	cfg := Config{
+		DBHost:     "mongodb://localhost:27017",
+		DBName:     "weather",
+		DBUser:     "mongo",
+		DBPassword: "password1",
+	}
+
 	log := log.New(os.Stdout, "WEATHER : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
-
-	if err := run(log); err != nil {
-		log.Println("main: error:", err)
-		os.Exit(1)
-	}
-}
-
-func run(log *log.Logger) error {
-	// =========================================================================
-	// Configuration
-	var cfg struct {
-		Web struct {
-			APIHost      string
-			ReadTimeout  time.Duration
-			WriteTimeout time.Duration
-			Handler      *mux.Router
-		}
-		DB struct {
-			User     string
-			Password string
-			Host     string
-			Name     string
-		}
-		Zipkin struct {
-			ReporterURI string
-			ServiceName string
-			Probability float64
-		}
-	}
 	db, err := database.Open(database.Config{
-		User:     cfg.DB.User,
-		Password: cfg.DB.Password,
-		Host:     cfg.DB.Host,
-		Name:     cfg.DB.Name,
+		User:     cfg.DBUser,
+		Password: cfg.DBPassword,
+		Host:     cfg.DBHost,
+		Name:     cfg.DBName,
 	})
 	if err != nil {
-		return errors.Wrap(err, "connecting to db")
+		log.Fatalf("%v connecting to db", err)
+		os.Exit(1)
 	}
 	defer func() {
-		log.Printf("main: Database Stopping : %s", cfg.DB.Host)
+		log.Printf("main: Database Stopping : %s", cfg.DBHost)
 		db.Close()
 	}()
-	return nil
 }
